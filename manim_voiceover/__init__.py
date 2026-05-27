0e314e18537e90e03e110cb86068a9873ca2230d
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2026-05-28 00:14:58 +0800

    chore: migrate packaging to uv

--- manim_voiceover/__init__.py
+++ manim_voiceover/__init__.py
@@ -1,6 +1,9 @@
 from manim_voiceover.tracker import VoiceoverTracker
 from manim_voiceover.voiceover_scene import VoiceoverScene
 
-import pkg_resources
+from importlib.metadata import PackageNotFoundError, version
 
-__version__: str = pkg_resources.get_distribution(__name__).version
+try:
+    __version__: str = version("manim-voiceover")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
